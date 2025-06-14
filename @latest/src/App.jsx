10331294dc14c279de6004e99758f1100c17473a
Uttamraj Singh <uttamrajsingh423@gmail.com>
2025-06-14 07:38:22 +0530

    added the functionalities

---
+++ @latest/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MusicPlayer from './components/MusicPlayer.jsx'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800 text-gray-300">
+      <MusicPlayer />
+    </div>
+  )
+}
+
+export default App
+
